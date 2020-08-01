c2a1fa396f67d9fa520115e2a1d0a08a913155e4
rob1104 <rescamilla@xis.mx>
2020-08-01 16:05:23 -0500

    Add routes needed

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/books', 'BookSController@store');
+Route::patch('/books/{book}', 'BookSController@update');
